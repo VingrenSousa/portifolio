577e2de3cd45781ab5037c8041aa07e90307d9ce
vingren Souza <vingren46@gmail.com>
2026-09-15 11:13:13 -0300

    bug de imagem profile no iphone

--- src/app/SectionAbout/styles.ts
+++ src/app/SectionAbout/styles.ts
@@ -57,7 +57,7 @@ export const Conteiner=styled.section<{visible:boolean}>`
                 >img{
              
                     width:90%;
-                    width: 300px;
+                    height: fit-content;
                    
                 }
             }
